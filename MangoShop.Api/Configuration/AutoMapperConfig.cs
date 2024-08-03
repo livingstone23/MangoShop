4bded2143ccc40dba9212da571e07389d4ae2c6e
Livingstone Cano <livingstone23@gmail.com>
2024-08-03 11:35:41 +0200

    Update DTO model in Api

---
+++ MangoShop.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MangoShop.Api.Dto;
+using MangoShop.Domain.Models;
+
+
+
+namespace MangoShop.Api.Configuration;
+
+
+
+/// <summary>
+/// Configuration class for AutoMapper profiles. 
+/// This class defines the mappings between domain entities and their corresponding DTOs (Data Transfer Objects).
+/// </summary>
+public class AutoMapperConfig : Profile
+{
+
+    /// <summary>
+    /// Constructor of AutoMapperConfig
+    /// </summary>
+    public AutoMapperConfig()
+    {
+
+        CreateMap<WhatsAppMessage, WhatsAppMessageDTO>();
+        CreateMap<WhatsAppMessageDTO, WhatsAppMessage>();
+
+
+    }
+
+
+}
