90d8cfa8468734337e13a8afa6291b54be282820
Livingstone Cano <lcano@sahgl.com>
2024-08-05 07:55:17 +0200

    Remove weatherForecast model

--- MangoShop.Api/Program.cs
+++ MangoShop.Api/Program.cs
@@ -24,7 +24,7 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 
 builder.Services.AddSwaggerGen(opts =>
 {
-    opts.SwaggerDoc("v1", new OpenApiInfo { Title = "API Test con WhatsApp V1.0", Version = "v1" });
+    opts.SwaggerDoc("v1", new OpenApiInfo { Title = "API Test V1.0", Version = "v1" });
 
     //Enable Annotations
     opts.EnableAnnotations();
